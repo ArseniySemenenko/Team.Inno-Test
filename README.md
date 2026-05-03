4822a6412929a68173fd6079724de3f02a194ee0
ArseniySemenenko <arseniysemenenko65@gmail.com>
2026-05-03 20:04:07 +0300

    Add README

---
+++ README.md
@@ -0,0 +1,18 @@
+Task: https://drive.google.com/file/d/1RBRcuH-_oAvtjem5Xs0c4NXZ8I38aYyH/view
+
+Link:
+https://arseniysemenenko.github.io/Team.Inno-Test/
+
+How to run the app:
+
+npm install
+npm run build
+npm run dev-server
+
+Deploy: GitHub pages (CI/CD)
+
+Add changes: 
+
+git add .
+git commit -m ""
+git push -u origin main
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
+  module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"], // Replaces style-loader
+        },
+      ],
+    },
+  devServer: {
+     static: './dist', // Folder to serve
+     hot: true,        // Enable HMR (default in v4+)
+     open: true        // Automatically open the browser
+   },
   mode: 'development',
   entry: './src/main.js',
   output: {
@@ -11,17 +24,21 @@ module.exports = {
     clean: true, // Cleans the dist folder before each build
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: 'index.html', // Path to your source HTML
     }),
     new CopyWebpackPlugin({
           patterns: [
             {
-              from: path.resolve(__dirname, 'public'),
+              from: path.resolve(__dirname, 'assets'),
               to: path.resolve(__dirname, 'dist/assets')
+        },
+        {
+          from: "./style.css",
+          to: path.resolve(__dirname, 'dist/')
             }
           ]
         }),
-  new Mo
   ],
 };
